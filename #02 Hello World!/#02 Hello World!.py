c7445f00b7c04504e6ec368adbe90506ca9fb8ee
murodjonabdujabborov <murodjon888abdujabborov@gmail.com>
2022-02-21 23:32:01 +0500

    yangi fayllar qo'shildi
    2 va 3 darslar yuklandi

---
+++ #02 Hello World!/#02 Hello World!.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 21 19:38:43 2022
+
+@author: Professional
+"""
+print("Assalomu Alaykum")
+
+print("Good Morning!")
+
+print(2+4*2)
+
+print(19/5)
+
+print(2**4)
---
+++ #03 Print(), Sintaksis va Arifmetik amallar/#03 Print(), Sintaksis va Arifmetik amallar.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 21 19:47:48 2022
+
+@author: Professional
+"""
+
+print("\"Nexia\", \"Tico\", 'Damas' ko'rganlar qilar havas")
+
+
+#1 5 ning 4-darajasi
+print("5 ning 4-darajasi", 5**4)
+
+
+#2 22 ni 4 ga bo'lganda qancha qoldiq qoladi?
+print("22 ni 4ga bo'lganda qoldiq", 22%4)
+
+
+#3 Tomonlari 125 ga teng kvadratning yuzi va peremetrini toping?
+print("Tomonlari 125 ga teng kvadratning yuzi", 125*125, "ga, peremetri", 4*125, "ga teng.")
+
+
+#4 Diametri 12 ga teng bo'lgan doiraning yuzini toping?
+print('Diametri 12 ga teng bo\'lgan doiraning yuzi', 3.14*(12/2)**2, "ga teng.")
+
+
+#5 Katetlari 6 va 7 bo'lgan to'g'ri burchakli uchburchakning gipotenuzasini toping?
+print("Katetlari 6 va 7 bo'lgan to'g'ri burchakli uchburchakning gipotenuzasi", (6**2+7**2)**(1/2))
+
+
+
